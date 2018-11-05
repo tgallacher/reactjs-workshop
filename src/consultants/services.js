5d73ccc31c5962b6a99a312149a92ebda54b984c
Tom Gallacher <tgallacher@users.noreply.github.com>
2018-11-05 21:12:11 +0000

    ADD/UPDATE basic dynamic version; served locally; ordered by team

--- src/consultants/services.js
+++ src/consultants/services.js
@@ -5,7 +5,7 @@
  * @returns {object}
  */
 export const fetchConsultantData = async () => {
-  const API_URL = ' https://demo3786895.mockable.io/consultants';
+  const API_URL = 'http://localhost:3000/consultants';
 
   const resp = await fetch(API_URL);
 
