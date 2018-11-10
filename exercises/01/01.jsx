8944c82d3cfa2e93c43eaa16e6a3c2157ce747c3
Tom Gallacher <tgallacher@users.noreply.github.com>
2018-11-10 12:52:38 +0000

    ADD first exercise

---
+++ exercises/01/01.jsx
@@ -0,0 +1,34 @@
+/* eslint react/destructuring-assignment: off */
+import React from 'react';
+
+// Exercise 01
+// ===========
+//
+// At this stage we are not interesting in layout, just
+// getting used to JSX and some of its features. Therefore,
+// don't worry about how the component renders/looks, focus
+// on making sure the right data is rendered.
+//
+// Tasks
+// --------
+// ✅    Your component should accept a 'status' prop, which renders the status label
+// ✅    Your component should accept a 'stat' prop, which renders a statistic
+// 🚫    Styling & layout are out of scope for this exercise
+//
+// Tip
+// ------
+// You can create an icon using the 'i' DOM node, and with either of
+// the following class names:
+//
+// Unavailable  =>    'fas fa-user-slash'
+// Available    =>    'far fa-user'
+// Busy         =>    'fas fa-user'
+function StatusBlock(props) {
+  return (
+    <div>
+      Add component implementation HERE!
+    </div>
+  );
+}
+
+export default StatusBlock;
---
+++ exercises/README.md
@@ -0,0 +1,20 @@
+# ReactJS Workshop: Exercises
+
+## 01 - ReactJS Elements, Components & Props
+## 02 - State, Lifecycle, & Handling Events
+## 03 - Conditional Rendering
+## 04 - Lists & Keys,
+## 05 - Controlled (Form) Components
+## 06 - CSS-in-JSS
+
+## 07 - HOCs
+## 08 - Render Props
+## 09 - Context API
+## 10 - Compound Components
+
+## 11 - Action Creators & Reducers
+## 12 - Selectors
+## 13 - Sagas
+## 14 -
+## 15 -
+## 16 -
