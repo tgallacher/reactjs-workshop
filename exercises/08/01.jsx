8d6e35eea819e1b8871b56f0961f0c4ca496df67
Tom Gallacher <tgallacher@users.noreply.github.com>
2018-11-11 22:07:45 +0000

    ADD test coverage for exercise 08/01

--- exercises/08/01.jsx
+++ exercises/08/01.jsx
@@ -14,13 +14,38 @@ import List from './components/01-ListComponent';
 // Exercise 08/01
 // ===========
 //
+// React's context API is a powerful tool in our bag of tricks for abstracting and sharing state across our components.
+// The context API also provides a streamlined mechanism for sharing this state across a deep DOM tree, without redundant
+// prop/concern "leaks". Although, caution should be taken -- as discussed during the presentation -- that while powerful,
+// the context API can limit the composability of components, and so other options should be weighed in order to choose
+// the right tool of the job.
+//
+// In this example, we will take a look at a slight deviation from the demo app, where we will render a list of team names,
+// with one of the items in the list being considered and highlighted as the "active" item. The styling of this item will
+// differ from the rest. We also include functionality so that we can change which item is active by clicking on the desired
+// item. Here, we will explore how the context API can simplify our component architecture.
+//
 // Tasks
 // --------
-// ✅    You should complete the `MousePosition` component so that it supports the render prop pattern
+// ✅    You should refactor the below components (some are in a different folder) to use React's Context API.
+// ✅    The context API should pass both the `activeIndex` value (type: number), and a `handleUpdateIndex`
+//        function which can be used to update the active index on a click event. The functions prototype should be:
+//        (newActiveIndex: number) => void
+// ✅    Your context provider should default the `activeIndex` to 0.
+// ✅    Your context provider should default the `handleUpdateIndex` to an empty function, that when called does nothing.
+// ✅    The `List` component should only accept 1 prop, the `values` prop, which contains the team names (type: string[])
+// ✅    The `ListItem` component should accept 2 props, the `value` prop and the `index` prop
+//        The `value` prop is the team name that the ListItem should render; its type is: string
+//        The `index` prop is the location in the list of this ListItem; its type is: number.
+//        All other required information should be obtained from the context API.
 //
 // Tips
 // ------
-// 🐨
+// 🐨   Use the `React.createContext` method to create your context components
+// 🐨   You can export and import anything that you need across the file/component modules!
+// 🐨   Don't forget to update each component's `propTypes` definition as you modify the props they support/require!
+export const ActiveContext = {};
+
 class App extends React.Component {
   static propTypes = {
     teamNames: PropTypes.string.isRequired,
--- exercises/Exercise08.story.jsx
+++ exercises/Exercise08.story.jsx
@@ -14,7 +14,9 @@ import { mount } from 'enzyme';
 import sinon from 'sinon';
 
 import CenterContent from './CenterContent';
-import Exercise01 from './08/01';
+import Exercise01, { ActiveContext } from './08/01';
+import ListItem from './08/components/01-ListItemComponent';
+import List from './08/components/01-ListComponent';
 
 // STOP!
 // =====
@@ -49,6 +51,114 @@ function renderExercise01() {
       {component}
     </CenterContent>
   );
+
+  specs(() => describe('01', () => {
+    let wrapper;
+    let ctxFn;
+
+    beforeEach(() => {
+      wrapper = mount(component);
+      ctxFn = sinon.fake();
+
+      mount(<ActiveContext.Consumer>{ctxFn}</ActiveContext.Consumer>);
+      wrapper = mount(component);
+    });
+
+    //
+    // Context
+    //
+    it('Context: Creates a context object with the expected shape', () => {
+      assert.property(ctxFn.lastCall.lastArg, 'activeIndex');
+      assert.property(ctxFn.lastCall.lastArg, 'handleUpdateIndex');
+    });
+
+    it('Context: Creates a context object with the expected parameter types', () => {
+      assert.isNumber(ctxFn.lastCall.lastArg.activeIndex);
+      assert.isFunction(ctxFn.lastCall.lastArg.handleUpdateIndex);
+    });
+
+    it('Context: Creates a context object with the expected default values', () => {
+      assert.propertyVal(ctxFn.lastCall.lastArg, 'activeIndex', 0);
+      assert.isUndefined(ctxFn.lastCall.lastArg.handleUpdateIndex());
+    });
+
+    //
+    // List
+    //
+    it('List: Has the expected propType definition', () => {
+      const supportedProps = Object.keys(List.propTypes); // eslint-disable-line
+
+      assert.lengthOf(supportedProps, 1);
+      assert.include(supportedProps, 'values');
+    });
+
+    //
+    // ListItem
+    //
+    it('ListItem: Has the expected propType definition', () => {
+      const supportedProps = Object.keys(ListItem.propTypes); // eslint-disable-line
+
+      assert.lengthOf(supportedProps, 2);
+      assert.include(supportedProps, 'value');
+      assert.include(supportedProps, 'index');
+    });
+
+    it('ListItem: Handles the expected input data correctly', () => {
+      const liWrapper = mount(<ListItem value="Foo" index={0} />);
+
+      assert.include(liWrapper.text(), 'Foo');
+    });
+
+    it('ListItem: Invokes the context\'s `handleUpdateIndex` as expected', () => {
+      const index = 3;
+      const fn = sinon.fake();
+      const context = {
+        // activeIndex: 2,
+        handleUpdateIndex: fn,
+      };
+
+      // FIXME Enzyme's Context option doesn't appear to
+      //  be working so wrap it around the provider.
+      const liWrapper = mount((
+        <ActiveContext.Provider value={context}>
+          <ListItem value="Foo" index={index} />
+        </ActiveContext.Provider>
+      ));
+
+      liWrapper.find('li').simulate('click');
+
+      assert.lengthOf(fn.lastCall.args, 1);
+      assert.isUndefined(fn.lastCall.args.target, 'The onClick handler should not pass a Synthetic event; but the index of the current item');
+      assert.equal(fn.lastCall.args[0], index, 'Expected the correct index to be sent through the context update handler');
+    });
+
+    it('ListItem: Styles the items according to their active state', () => {
+      const index = 1;
+      const context = {
+        activeIndex: 2,
+      };
+
+      // FIXME Enzyme's Context option doesn't appear to
+      // be working so wrap it around the provider.
+      let liWrapper = mount((
+        <ActiveContext.Provider value={context}>
+          <ListItem value="Foo" index={index} />
+        </ActiveContext.Provider>
+      ));
+
+      assert.isTrue(liWrapper.find('li').hasClass('bg-purple-light'), 'Expected the active state to have a light PURPLE bg');
+
+      // FIXME Again, Enzyme's Context API doesn't seem to working
+      // Updating props, clears the context...
+      liWrapper = mount((
+        <ActiveContext.Provider value={context}>
+          <ListItem value="Foo" index={context.activeIndex} />
+        </ActiveContext.Provider>
+      ));
+
+      assert.isTrue(liWrapper.find('li').hasClass('bg-teal-light'), 'Expected the active state to have a light TEAL bg');
+    });
+  }));
 
   return story;
 }
