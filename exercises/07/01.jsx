04064a77a3f02e73e2d6bc6d53695d8c81db1549
Tom Gallacher <tgallacher@users.noreply.github.com>
2018-11-11 17:00:26 +0000

    ADD exercise 07/01

---
+++ exercises/07/01.jsx
@@ -0,0 +1,71 @@
+/* eslint
+  react/destructuring-assignment: off,
+  react/jsx-one-expression-per-line: off,
+  react/prefer-stateless-function: off,
+  react/require-default-props: off,
+  react/no-multi-comp: off,
+  max-len: off,
+*/
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+// Exercise 07/01
+// ===========
+//
+// Now let's look at an alternative component pattern which can also be used to cross-cut concerns
+// between child components, namely the "Render Prop" pattern. While the demo app doesn't currently
+// define any Render Props directly, the Render Prop pattern is one of the most important patterns
+// which are in use today for abstracting and reusing compnent state/logic.
+//
+// We will repeat the HOC exercise on capturing and displaying the current mouse position, but this
+// time, instead of using a HOC, we'll use a render prop.
+//
+// Tasks
+// --------
+// ✅    You should complete the `MousePosition` component so that it supports the render prop pattern
+// ✅    Your `MousePosition` component should listen/capture current mouse position, as we did in the HOC exercise
+// ✅    Your `MousePosition` should offer the current mouse position coords as a single object parameter to any child components
+// ✅    Your `MousePosition` component should update the current coordinates in real time, as the user moves their mouse
+// ✅    Your component include the `MousePosition` component
+// ✅    Your component should render the current X, Y position. The format should be: (x,y) including the parentheses, as before.
+//
+// 🚫   Styling is out of scope
+// 🚫   You are free to add additional text, markup, but we will only be looking for the current X, Y position of the mouse
+//
+// Tips
+// ------
+// 🐨  Use the `onMouseMove` Synthetic DOM event to capture the current X and Y coords of the mouse
+// 🐨  You solution to the HOC exercise could be a good starting point
+export class MousePosition extends React.Component {
+  render() {
+    return (
+      <div>
+        Start HERE (Render Prop Component)!
+      </div>
+    );
+  }
+}
+
+// Define display name so we can select in our tests
+MousePosition.displayName = 'MousePosition';
+
+// Add runtime prop validation
+MousePosition.propTypes = {
+  children: PropTypes.func,
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Start HERE!
+      </div>
+    );
+  }
+}
+
+// Add runtime prop validation
+App.propTypes = {};
+
+export default App;
---
+++ exercises/Exercise07.story.jsx
@@ -0,0 +1,136 @@
+/* eslint
+  import/no-extraneous-dependencies: off,
+  no-use-before-define: off,
+  object-curly-newline: off,
+  func-names: off,
+  max-len: off,
+*/
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { specs, describe, it } from 'storybook-addon-specifications';
+import { beforeEach, afterEach, after } from 'storybook-addon-specifications/dist/preview';
+import { assert } from 'chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+
+import CenterContent from './CenterContent';
+import Exercise01, { MousePosition } from './07/01';
+
+// STOP!
+// =====
+//
+// This file is not intended to be edited. This is used to
+// help display and validate the work on the exercises.
+//
+// In general, files ending with /*.story.jsx$/i are not expected to be changed
+// as part of the exercises.
+//
+// @see: All files inside './05/*' instead.
+//
+
+storiesOf('Exercises/07', module)
+  .add('01', renderExercise01);
+
+//
+// Exercise 01
+//
+function renderExercise01() {
+  const component = <Exercise01 />;
+  const story = (
+    <CenterContent>
+      <div className="border border-purple border-dashed">
+        {component}
+      </div>
+    </CenterContent>
+  );
+
+  specs(() => describe('01', () => {
+    let renderPropFn;
+    let wrapper;
+    let mpWrapper;
+
+    beforeEach(() => {
+      renderPropFn = sinon.fake();
+
+      wrapper = mount(component);
+      mpWrapper = mount(<MousePosition>{renderPropFn}</MousePosition>);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+      wrapper.unmount();
+      wrapper = null;
+    });
+
+    //
+    // MousePosition
+    //
+    it('MousePosition: calls the render prop with 1 arg', () => {
+      assert.lengthOf(renderPropFn.lastCall.args, 1, 'Expected render prop to be called with 1 arg');
+    });
+
+    it('MousePosition: calls the render prop with an object containg the keys x, y', () => {
+      assert.property(renderPropFn.lastCall.lastArg, 'x');
+      assert.property(renderPropFn.lastCall.lastArg, 'y');
+    });
+
+    it('MousePosition: Defaults the mouse coords to (0,0)', () => {
+      const rpFn = renderPropFn.lastCall;
+      const expected = { x: 0, y: 0 };
+
+      assert.isObject(rpFn.lastArg, 'Expected render prop to be called with an object');
+      assert.deepEqual(renderPropFn.lastCall.lastArg, expected, 'Expected render porp defalut mouse to (0,0)');
+    });
+
+    // Slight duplication with below; but here we are testing the MousePosition component
+    // updates, and outputs the change. Later, we are testing that the component updates the display to the change.
+    it('MousePosition: handles and propagates mouse events', () => {
+      const x = 12;
+      const y = 982;
+      const expected = { x, y };
+
+      mpWrapper.simulate('mousemove', { clientX: x, clientY: y });
+      assert.deepEqual(renderPropFn.lastCall.lastArg, expected, 'Expected render prop to have updated due to mouse event');
+    });
+
+    //
+    // APP
+    //
+    it('App: Includes the `MousePosition` component', () => {
+      assert.lengthOf(
+        wrapper.find('MousePosition'),
+        1,
+        'Expected to find a single use of the `MousePosition` component',
+      );
+    });
+
+    it('App: Does not use a `mouse` prop', () => {
+      const x = 123;
+      const y = 345;
+
+      wrapper.setProps({ mouse: { x, y } });
+
+      assert.notInclude(wrapper.text().replace(/\s/g, ''), `(${x},${y})`);
+      assert.notInclude(wrapper.text(), 'Start HERE!', 'Appears the component hasn\'t been edited yet');
+    });
+
+    // Slight duplication with above; but here we are testing that the component updates the
+    // display to the change. Above, we are testing the MousePosition component updates, and
+    // outputs the change.
+    it('App: Continually re-renders the correct mouse (X,Y) coords', () => {
+      const events = [
+        { x: 123, y: 234 },
+        { x: 283, y: 463 },
+        { x: 837, y: 2 },
+      ];
+
+      events.forEach(({ x, y }) => {
+        wrapper.find('MousePosition').simulate('mousemove', { clientX: x, clientY: y });
+
+        assert.include(wrapper.text().replace(/\s/g, ''), `(${x},${y})`);
+      });
+    });
+  }));
+
+  return story;
+}
