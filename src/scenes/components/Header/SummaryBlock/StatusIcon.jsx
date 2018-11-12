8aa0c930d7f83ea585f2bf4b38115951acbe55cf
Tom Gallacher <tgallacher@users.noreply.github.com>
2018-11-12 12:34:37 +0000

    UPDATE display name for summary block components to fix weird testing bug

--- src/scenes/components/Header/SummaryBlock/StatusIcon.jsx
+++ src/scenes/components/Header/SummaryBlock/StatusIcon.jsx
@@ -1,14 +1,19 @@
+/* eslint
+  react/require-default-props: off,
+*/
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const StatusIcon = ({ iconClassName }) => (
+const StatusIcon = ({ iconClassName = '' }) => (
   <div>
     <i className={iconClassName} />
   </div>
 );
 
 StatusIcon.propTypes = {
-  iconClassName: PropTypes.string.isRequired,
+  iconClassName: PropTypes.string,
 };
+
+StatusIcon.displayName = 'StatusIcon';
 
 export default StatusIcon;
--- src/scenes/components/Header/SummaryBlock/StatusLabel.jsx
+++ src/scenes/components/Header/SummaryBlock/StatusLabel.jsx
@@ -1,14 +1,19 @@
+/* eslint
+  react/require-default-props: off,
+*/
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const StatusLabel = ({ label }) => (
+const StatusLabel = ({ label = '' }) => (
   <div className="text-xs p-0 m-0">
     {label.charAt(0).toUpperCase() + label.substr(1).toLocaleLowerCase()}
   </div>
 );
 
 StatusLabel.propTypes = {
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
+
+StatusLabel.displayName = 'StatusLabel';
 
 export default StatusLabel;
