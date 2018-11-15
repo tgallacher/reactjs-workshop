47d10e058dd0957f8dcb08f709178ac02ff133d4
Tom Gallacher <tgallacher@users.noreply.github.com>
2018-11-15 09:18:59 +0000

    ADD initial ex 10 story, with app invoktion

---
+++ exercises/stories/Exercise10.story.jsx
@@ -0,0 +1,62 @@
+ /* eslint
+  import/no-extraneous-dependencies: off,
+  no-use-before-define: off,
+  object-curly-newline: off,
+  func-names: off,
+  max-len: off,
+*/
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { specs, describe, it } from 'storybook-addon-specifications';
+// import { beforeEach, afterEach } from 'storybook-addon-specifications/dist/preview';
+import { assert } from 'chai';
+// import sinon from 'sinon';
+
+import CenterContent from '../CenterContent';
+import store, { fetchConsultantComplete } from '../09/01';
+import { rowData } from './Exercise04.story';
+import AppWrapper from '../10/01';
+import App, { mapStateToProps } from '../10/App';
+
+// STOP!
+// =====
+//
+// This file is not intended to be edited. This is used to
+// help display and validate the work on the exercises.
+//
+// In general, files ending with /*.story.jsx$/i are not expected to be changed
+// as part of the exercises.
+//
+// @see: All files inside '../08/*' instead.
+//
+
+storiesOf('Exercises/10', module)
+  .add('01 - React Redux + mapStateToProps', renderExercise01);
+
+function renderExercise01() {
+  let story = (
+    <CenterContent>
+      <h1>Please complete Exercise 09/01 first</h1>
+    </CenterContent>
+  );
+
+  if (store != null) {
+    if (! fetchConsultantComplete || fetchConsultantComplete() == null) {
+      throw new Error('You should make sure your tests pass in Exercise 09/01 for the `fetchConsultantComplete` action creator');
+    }
+
+    store.dispatch(fetchConsultantComplete(rowData));
+
+    story = (
+      <CenterContent>
+        <AppWrapper />
+      </CenterContent>
+    );
+  }
+
+  specs(() => describe('', () => {
+
+  }));
+
+  return story;
+}
