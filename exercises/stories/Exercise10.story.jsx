632ab9ee13ed31e4282ad88d17c6059cfc3e72be
Tom Gallacher <tgallacher@users.noreply.github.com>
2018-11-15 09:43:42 +0000

    ADD initial test suite for exercise 10

--- exercises/stories/Exercise10.story.jsx
+++ exercises/stories/Exercise10.story.jsx
@@ -10,6 +10,7 @@ import { storiesOf } from '@storybook/react';
 import { specs, describe, it } from 'storybook-addon-specifications';
 // import { beforeEach, afterEach } from 'storybook-addon-specifications/dist/preview';
 import { assert } from 'chai';
+import { shallow } from 'enzyme';
 // import sinon from 'sinon';
 
 import CenterContent from '../CenterContent';
@@ -55,7 +56,31 @@ function renderExercise01() {
   }
 
   specs(() => describe('', () => {
+    it('mapStateToProps: Is defined and is callable', () => {
+      assert.isNotNull(mapStateToProps, 'mapStateTopProps should be defined');
+      assert.isFunction(mapStateToProps, 'mapStateTopProps should be callable');
+    });
 
+    it('mapStateToProps: should map the data to the `consultantData` prop', () => {
+      assert.isFunction(mapStateToProps, 'mapStateTopProps should be callable');
+
+      const mockData = [
+        { name: 'foo' },
+        { name: 'bar' },
+      ];
+
+      const props = mapStateToProps(mockData);
+
+      assert.hasAnyKeys(props, 'consultantData', 'Your selector should assign data to the `consultantData` prop key');
+      assert.deepEqual(props, mockData, 'Your selector should pull the correct data from the store');
+      assert.strictEqual(props, mockData, 'Your selector should pull the exact same data from the store');
+    });
+
+    it('App: Connects it to the Redux store', () => {
+      const wrapper = shallow(<App store={store} />);
+
+      assert.isDefined(wrapper.props().dispatch, 'Expected to see a connected `App` component');
+    });
   }));
 
   return story;
