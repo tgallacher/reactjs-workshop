c1ac9fabe9e26dd51cd22d3aa7a8903c2386e312
Tom Gallacher <tgallacher@users.noreply.github.com>
2018-11-20 22:55:41 +0000

    UPDATE Appbar with semantic component names using EmotionJS wrapper

--- src/AppBar/index.jsx
+++ src/AppBar/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import styled, { cx } from 'react-emotion';
+
+const Header = styled('header')``;
+const ContentWrapper = styled('div')``;
+const PageHeadingArea = styled('div')``;
+const LogoutArea = styled('div')``;
+const Logout = styled('button')``;
 
 const AppBar = () => (
-  <header className="bg-white font-sans">
-    <div className="flex max-w-3xl mx-auto h-10 items-center">
-      <div className="flex-1 text-grey-darker text-xl">
+  <Header className="bg-white font-sans">
+    <ContentWrapper className="flex max-w-3xl mx-auto h-10 items-center">
+      <PageHeadingArea className="flex-1 text-grey-darker text-xl">
         Team Status Summary
-      </div>
-      <div className="">
-        <button className="bg-red hover:bg-red-dark text-white font-bold py-2 px-2">
+      </PageHeadingArea>
+
+      <LogoutArea>
+        <Logout className="bg-red hover:bg-red-dark text-white font-bold py-2 px-2">
           <i className="fa fa-power-off"></i>
-        </button>
-      </div>
-    </div>
-  </header>
+        </Logout>
+      </LogoutArea>
+    </ContentWrapper>
+  </Header>
 );
 
 export default AppBar;
